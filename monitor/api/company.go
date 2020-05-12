0254c89752cde0498bf9d945900ba7e572865eec
zpbird <zpbird@qq.com>
2020-05-12 13:51:58 +0800

    m

--- monitor/api/company.go
+++ monitor/api/company.go
@@ -7,13 +7,25 @@ import (
 	"os/exec"
 )
 
+// 定义模板文件常量
+const (
+	sumTemplate   = "汇总.xlsx"
+	videoTemplate = "录像明细.xlsx"
+)
+
 // GetCompany 需要传入模板文件夹的路径，返回选中的公司名称...
 func GetCompany(templateDir string) string {
+
 	// 获取模板文件夹下的公司名称列表
-	if dirList := SubPathList(templateDir); len(dirList) == 0 {
+	dirList := SubPathList(templateDir)
+	var getNum int
+	if len(dirList) == 0 {
 		fmt.Println("模板文件夹：template 下没有任何文件夹，请按公司名称设置对应的文件夹！")
 		os.Exit(0)
-	} else {
+	}
+
+	// 选择公司
+	for getNum < 1 || getNum > len(dirList) {
 		// fmt.Printf("\x1bc") //清屏
 		cmd := exec.Command("cmd.exe", "/c", "cls") //windows清屏命令
 		cmd.Stdout = os.Stdout
@@ -26,12 +38,20 @@ func GetCompany(templateDir string) string {
 			fmt.Printf(" %d. %s\n", key+1, value)
 		}
 		fmt.Printf("\n")
-		fmt.Println("( 1 -", len(dirList), ") : ")
+		fmt.Print("[1 - ", len(dirList), "] : ")
 
+		fmt.Scanln(&getNum)
 	}
 
-	// 选择公司
-
 	// 检查选中公司对应的模板文件是否存在
-	return "hello"
+	if b1, _ := FileExists(templateDir + "/" + dirList[getNum-1] + "/" + sumTemplate); !b1 {
+		fmt.Println("该公司模板文件缺失！检查\"汇总.xlsx\"和\"录像明细.xlsx\"是否存在。")
+		os.Exit(0)
+	} else if b2, _ := FileExists(templateDir + "/" + dirList[getNum-1] + "/" + videoTemplate); !b2 {
+		fmt.Println("该公司模板文件缺失！检查\"汇总.xlsx\"和\"录像明细.xlsx\"是否存在。")
+		os.Exit(0)
+	}
+
+	// 返回结果
+	return dirList[getNum-1]
 }

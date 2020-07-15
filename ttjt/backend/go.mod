86602a9c1709711ad94c96bb7cbae81c97240e17
zpbird <zpbird@qq.com>
2020-07-15 17:34:51 +0800

    m

--- ttjt/backend/go.mod
+++ ttjt/backend/go.mod
@@ -2,4 +2,4 @@ module backend
 
 go 1.14
 
-require github.com/360EntSecGroup-Skylar/excelize v1.4.1
+require github.com/gin-gonic/gin v1.6.3
--- ttjt/backend/main.go
+++ ttjt/backend/main.go
@@ -1,24 +1,13 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/360EntSecGroup-Skylar/excelize"
-)
+import "github.com/gin-gonic/gin"
 
 func main() {
-	f := excelize.NewFile()
-	// 创建一个工作表
-	index := f.NewSheet("Sheet2")
-	// 设置单元格的值
-	f.SetCellValue("Sheet2", "A2", "Hello world.")
-	f.SetCellValue("Sheet1", "B2", 100)
-	// 设置工作簿的默认工作表
-	f.SetActiveSheet(index)
-	// 根据指定路径保存文件
-	if err := f.SaveAs("Book1.xlsx"); err != nil {
-		println(err.Error())
-	}
-	fmt.Printf("%s\n", "good")
-
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080") http://localhost:8080/ping
 }
